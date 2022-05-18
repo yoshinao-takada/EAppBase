5a498618466364118dea2726028610919d2843ee
Yoshinao Takada <yoshinao_takada2002@yahoo.co.jp>
2022-05-19 08:32:58 +0900

    NoBind project was added. It is a sample which does not use data binding but is robust to future specification change.

---
+++ NoBind/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NoBind
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IMainWindowExports _windowAPI = null;
+        MainWindow _mainWindow;
+        public void ButtonAction(object sender, RoutedEventArgs e)
+        {
+            int i = _windowAPI.Counter;
+            i++;
+            _windowAPI.Counter = i;
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _mainWindow = new MainWindow();
+            _mainWindow.Show();
+            _windowAPI = _mainWindow.MyInterface; // Get the published interface of MainWindow class.
+            _windowAPI.ButtonAction += ButtonAction; // register my own button action to the main window.
+        }
+    }
+
+
+}
---
+++ NoBind/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NoBind
+{
+    public interface IMainWindowExports
+    {
+        event RoutedEventHandler ButtonAction;
+
+        int Counter { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IMainWindowExports
+    {
+        Random rand;
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+
+        Label[] Labels = new Label[6];
+
+        #region IMainWindowExports_Impl
+        event RoutedEventHandler IMainWindowExports.ButtonAction
+        {
+            add
+            {
+                MyButton.Click += value;
+            }
+            remove
+            {
+                MyButton.Click -= value;
+            }
+        }
+
+
+        private int _counter = 0;
+        public int Counter
+        {
+            get => _counter;
+            set
+            {
+                _counter = value;
+                int _counterMod6 = _counter % 6;
+                byte[] byteBuffer = new byte[3];
+                rand.NextBytes(byteBuffer);
+                var _color = System.Windows.Media.Color.FromRgb(byteBuffer[0], byteBuffer[1], byteBuffer[2]);
+                Labels[_counterMod6].Background = new SolidColorBrush(_color);
+                ShowCounter.Content = _counter.ToString();
+            }
+        }
+        #endregion
+
+        public IMainWindowExports MyInterface
+        {
+            get => this as IMainWindowExports;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Labels[0] = Label0;
+            Labels[1] = Label1;
+            Labels[2] = Label2;
+            Labels[3] = Label3;
+            Labels[4] = Label4;
+            Labels[5] = Label5;
+        }
+    }
+}
